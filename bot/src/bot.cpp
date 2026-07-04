deac4b910b75c26047e0865a7f275c2461a34f0f
batmvn <paulomartin012@gmail.com>
2026-07-05 02:53:25 +0300

    forward messages to chat

--- bot/src/bot.cpp
+++ bot/src/bot.cpp
@@ -3,6 +3,7 @@
 #include<tgbot/Api.h>
 #include<string>
 #include<stdio.h>
+#include <iostream>
 #include<tgbot/net/TgLongPoll.h>
 using namespace TgBot;
 int main(){
@@ -14,8 +15,13 @@ bot.getEvents().onCommand("start",[&api](Message::Ptr message){
  api.sendMessage(message->chat->id,"hi am "+me->username );
  });
 bot .getEvents().onAnyMessage([&api](Message::Ptr message){
-    api.sendMessage(message->chat->id,"mesage-id "+std::to_string(message->messageId));
+ /*   api.sendMessage(message->chat->id,"mesage-id "+std::to_string(message->messageId));
     api.sendMessage(message->chat->id,"chat-id "+std::to_string(message->chat->id));
+*/
+    std::cout<<"chat-id "<<message->chat->id<<std::endl;
+    std::  cout<<"user-id "<<message->from->id<<std::endl;
+    api.forwardMessage(getenv("chat"),message->chat->id,message->messageId,true,true);
+   
     });
 TgLongPoll longpoll(bot);
 while(true){
